19cb472ff320b08aea2a9008b14d775a8089f5d7
muqeetkhan <muqeetkhan050@gmail.com>
2025-09-06 23:39:21 +1000

    add changes

--- src/components/api/Chat/Route.js
+++ src/components/api/Chat/Route.js
@@ -5,3 +5,6 @@ import {GoogleGenerativeAI} from '@langchain/google-genai';
 import {GoogleGenerativeAIEmbeddings} from '@langchain/google-genai';
 import {TaskType} from '@langchain/google-genai';
 import {QdrenaVectorStore} from '@@langchain/qdrant';
+import {ChatVectorDBQAChain} from 'langchain/chains';
+import {useState} from 'react';
+
